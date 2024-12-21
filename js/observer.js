a61cde64595f09b899322ab0877fe5258738ea36
MAJI KWA WATOTO FOUNDATIO <mrbayamwaro@gmail.com>
2024-12-21 13:41:39 +0300

    OBSERVOR-ANIMATIONS

--- js/observer.js
+++ js/observer.js
@@ -4,14 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < entries.length; i++) {
       const entry = entries[i];
       if (entry.isIntersecting) {
-        entry.target.classList.add('scrolled'); // Add the animation class
-        observer.unobserve(entry.target); // Stop observing once animated
+      //   entry.target.classList.add('scrolled'); // Add the animation class
+      //   observer.unobserve(entry.target); // Stop observing once animated
+      if(entry.target.classList.contains('slide-up')){
+        entry.target.classList.add('scrolled');
+      }else if(entry.target.classList.contains('from-left')){
+        entry.target.classList.add('slide-left');
+      }else if(entry.target.classList.contains('from-right')){
+        entry.target.classList.add('slide-right');
+      }else if(entry.target.classList.contains('go-up')){
+        entry.target.classList.add('fade-scale-up')
       }
+      observer.unobserve(entry.target);
+      }
+      
     }
   });
 
   // Select all elements you want to animate
-  const elementsToAnimate = document.querySelectorAll('.slide-up');
+  const elementsToAnimate = document.querySelectorAll('.animated');
 
   // Observe each element using a for loop
   for (let i = 0; i < elementsToAnimate.length; i++) {
